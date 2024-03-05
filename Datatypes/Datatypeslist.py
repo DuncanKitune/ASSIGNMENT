46b6b2230fbcf5789fe25adeb2285091cfc88ef3
Duncan Kitune <dkcconsultingltd@gmail.com>
2024-03-05 16:43:54 +0300

    We have updated some items eg meta details and added new assignment files.

---
+++ Datatypes/Datatypeslist.py
@@ -0,0 +1,14 @@
+list = ('abcd', 786,2.23 ,'John', 70.2)
+tinylist =[123, 'John']
+print(list)
+print (tinylist)
+print(list[2:3])
+
+print(tinylist*3)
+tuple = 'abcd',768,2.223,'John',70.2
+print(tuple)
+print(tuple[1:3])
+print(tuple*2)
+print(tuple[4:]*3)
+tinytuple = 'Wed', 'Sunday'
+print(tuple+tinytuple)
