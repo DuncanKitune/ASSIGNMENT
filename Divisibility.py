de3b3c8025c9fa6f4d2a2f4e31d7c0e81eec9e16
Duncan Kitune <dkcconsultingltd@gmail.com>
2024-03-19 12:07:41 +0300

    -m Adjustments

--- Divisibility.py
+++ Divisibility.py
@@ -1,12 +1,6 @@
 #Function to check if a number is divisible by ten
-def check_divisibility_by_ten():
-    num = int(input("Enter a number to check if its divisible by ten: "))
-    if divisible_by_ten():
-        print(f"{num} is divisible by ten.")
-    else:
-        print(f"{num} is not divisible by ten")
-
 def divisible_by_ten(num):
+    num = int(input("Enter a number to check if its divisible by ten: "))
     if num % 10 == 0:
         return True
     else:
@@ -15,4 +9,3 @@ print("\nTest cases for divisible_by_ten function:")
 print(divisible_by_ten(25))  # False
 print(divisible_by_ten(100)) # True
 
-# Function to prompt user input and check divisibility by 10
--- week3discount&functions.py
+++ week3discount&functions.py
@@ -1,5 +1,6 @@
 # Function to calculate discount
 def calculate_discount(price, discount_percent):
+ 
     if discount_percent >= 20:
         discounted_price = price - (price * (discount_percent / 100))
         return discounted_price
@@ -8,14 +9,10 @@ def calculate_discount(price, discount_percent):
 
 # Prompting user input and applying discount
 def apply_discount():
-    original_price = float(input("Enter the original price of the item: $"))
+    price = float(input("Enter the original price of the item: $"))
     discount_percent = float(input("Enter the discount percentage: "))
-    final_price = calculate_discount(original_price, discount_percent)
+    final_price = calculate_discount(price, discount_percent)
     print(f"The final price after applying the discount is: ${final_price:.2f}")
-
-
-
-#
 
 # Applying discount based on user input
 apply_discount()
